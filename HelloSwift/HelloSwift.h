c03b91193cf2229a72aa74023c7ae5553d024f56
Test <ios@trioangle.com>
2022-02-15 15:20:39 +0530

    test Push

---
+++ HelloSwift/HelloSwift.h
@@ -0,0 +1,18 @@
+//
+//  HelloSwift.h
+//  HelloSwift
+//
+//  Created by trioangle on 15/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloSwift.
+FOUNDATION_EXPORT double HelloSwiftVersionNumber;
+
+//! Project version string for HelloSwift.
+FOUNDATION_EXPORT const unsigned char HelloSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloSwift/PublicHeader.h>
+
+
